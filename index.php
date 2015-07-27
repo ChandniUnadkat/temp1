b1ce8b3eaa6278a5754aeeec299e243b975ff37a
unknown <ChandniMehta>
2015-07-27 15:25:37 +0100

    File added

--- index.php
+++ index.php
@@ -1,78 +1,94 @@
-<?php
-
-	$database = new SQLitedatabase('myDatabase.db');
-
- if(isset($_POST['submit1']))
- 	{
-	 	echo"<br>First Name =".$_FILES["file1"]["name"];
-		echo"<br>Temporary File Name =".$_FILES["file1"]["tmp_name"];
-		echo"<br>File Size =".$_FILES["file1"]["size"];
-		move_uploaded_file($_FILES["file1"]["tmp_name"],"upload//".$_FILES["file1"]["name"]);
-	}
+<?php 
+   require('main_db_class.php'); 
+   $obj = new main_class;
+   $ret = $obj->displayMembers();
 ?>
-
-<script>
-function validateForm1() 
-	{
-    	var a = document.forms["myForm"]["file1"].value;
-    	if (a == null || a == "")
-		{
-        	alert("Can't upload without file attachment !!");
-        return false;
-    	}
-	}
-	
-var _validFileExtensions = [".csv"];    
-
-function ValidateSingleInput(oInput) 
-	{
-   	 if (oInput.type == "file") {
-        var sFileName = oInput.value;
-         if (sFileName.length > 0)
-		  {
-            var blnValid = false;
-            for (var j = 0; j < _validFileExtensions.length; j++)
-			 {
-                var sCurExtension = _validFileExtensions[j];
-                if (sFileName.substr(sFileName.length - sCurExtension.length, sCurExtension.length).toLowerCase() == sCurExtension.toLowerCase()) 
-				{
-                    blnValid = true;
-                    break;
-                }
-            }
-             
-            if (!blnValid)
-			 {
-                alert("Sorry, " + sFileName + " is invalid, allowed extensions are: " + _validFileExtensions.join(", "));
-                oInput.value = "";
-                return false;
-            }
-        }
-    }
-    return true;
-}	
-</script>
-
-<!DOCTYPE html>
 <html>
-<head>
-	<title>First Task</title>
-</head>
-<body>
-	<center>
-		<h2>First Task </h2>
-			<form name="myForm" action="index.php" method="post" onsubmit="return validateForm1()" enctype="multipart/form-data">
-			<table border="3" >
-			<tr>
-			<td>
-				Select CSV File to Upload : </td>
-					<td><input type="file" name="file1" onchange="ValidateSingleInput(this);"/> </td></tr>
-
-					<tr><td><td><input type="submit" name="submit1" value="Upload" />
-			</td></td>
-			</tr>
-			</table>		
+    <title>First Task</title>
+	<head>
+		<script>
+			function validateForm1() 
+				{
+					var a = document.forms["myForm"]["file1"].value;
+					
+					if (a == null || a == "")
+					{
+						alert("Warning : Can't upload without file attachment !!");
+					return false;
+					}
+				}
+				
+			var _validFileExtensions = [".csv"];    
+			
+			function ValidateSingleInput(oInput) 
+				{
+				 if (oInput.type == "file") {
+					var sFileName = oInput.value;
+					 if (sFileName.length > 0)
+					  {
+						var blnValid = false;
+						for (var j = 0; j < _validFileExtensions.length; j++)
+						 {
+							var sCurExtension = _validFileExtensions[j];
+							if (sFileName.substr(sFileName.length - sCurExtension.length, sCurExtension.length).toLowerCase() == sCurExtension.toLowerCase()) 
+							{
+								blnValid = true;
+								break;
+							}
+						}
+						 
+						if (!blnValid)
+						 {
+							alert("Sorry, " + sFileName + " is invalid. Allowed extensions are: " + _validFileExtensions.join(", ") + " only !");
+							oInput.value = "";
+							return false;
+						}
+					}
+				}
+				return true;
+			}	
+			</script>
+ 	</head>
+    <body>
+		<center><h4>First Task</h4>
+         <form name="myForm" action="index.php" method="post" enctype="multipart/form-data" onSubmit="return validateForm1()">
+		 	<table border="2">
+				<thead>Upload CSV File</thead>
+					<tr>
+						<td><input type="file" name="file1" onChange="ValidateSingleInput(this)"></td>
+						<td><input type="submit" value="Upload File" name="submit"></td>
+					</tr>
+			</table>
 			</form>
-	</center>
-</body>
+			<br /><br />
+			<table border="2">
+       			<thead>
+					<tr>
+						<th>Staff Member Name</th>
+						<th>Payroll Reference</th>
+						<th>Total Hours Worked</th>
+						<th>Total Pay</th>
+						<th>Work Date</th>  
+					</tr>
+        		</thead>
+       		 <tbody align="center">
+				<?php
+				while($row = $ret->fetchArray() )
+				{
+				?>
+					<tr>
+						<td><?php echo $row['First_Name'].' '.$row['Family_Name'].' '.$row['Middle_Name'] ?></td>
+						<td><?php echo $row['Payroll_Reference'];?></td>
+						<td><?php echo $row['Total_Hours_Worked'];?></td>
+						<td><?php echo $row['Total_Pay'];?></td>
+						<td><?php echo date('dS F Y',$row['Work_Date']);?>
+						</td>
+					</tr>
+					 
+			   <?php        
+				}
+			   ?>
+       		</tbody>
+		</table>
+     </body></center>
 </html>
